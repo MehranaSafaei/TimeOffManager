39d1ea0db659ffc16905ef65b8c64a509eecb9e7
mehrana <mehrana.safaei3@gmail.com>
2025-01-21 10:01:40 +0330

    add namedQuery findById

--- src/main/java/org/example/entity/Personnel.java
+++ src/main/java/org/example/entity/Personnel.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "personnel")
 //@SecondaryTables({@SecondaryTable(name = "").@SecondaryTable(name = "")})
-@NamedQuery(name = "selectAll", query = "SELECT Personnel FROM Personnel")
+@NamedQueries({@NamedQuery(name = "selectAll", query = "SELECT Personnel FROM Personnel"),
+@NamedQuery(name = "findById", query = "SELECT Personnel FROM Personnel WHERE Personnel.id= :id")})
 public class Personnel extends AbstractPersonnel{
 }
